8fc4817e67fff34a47f0d539b65cf53c509eecae
SaulRC1 <saul.rodriguez.naranjo@gmail.com>
2024-05-23 00:50:12 +0200

    Práctica 3 - Establecimiento inicial

---
+++ Practica 3 - C# Net Remoting/GestorBibliotecaService/GestorBibliotecaClient/GestorBibliotecaClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorBibliotecaService
+{
+    class GestorBibliotecaClient
+    {
+        static void Main(string[] args)
+        {
+            ChannelServices.RegisterChannel(new TcpChannel(), false);
+
+            GestorBibliotecaService gestorBiblioteca = (GestorBibliotecaService)Activator.GetObject(typeof(GestorBibliotecaService),
+                "tcp://localhost:9000/GestorBiblioteca");
+
+
+
+        }
+    }
+}
---
+++ Practica 3 - C# Net Remoting/GestorBibliotecaService/GestorBibliotecaServer/GestorBibliotecaServer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorBibliotecaService
+{
+    class GestorBibliotecaServer
+    {
+        static void Main(string[] args)
+        {
+            ChannelServices.RegisterChannel(new TcpChannel(9000), false);
+            Console.WriteLine("Registrando el servicio de Gestor Bibliotecario en modo Singleton...");
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(GestorBibliotecaService), "GestorBiblioteca",
+                WellKnownObjectMode.Singleton);
+
+            Console.WriteLine("Esperando llamadas remotas...");
+            Console.WriteLine("Pulsa enter para salir...");
+            Console.ReadLine();
+        }
+    }
+}
